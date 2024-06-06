efb461286bc077f3b18b7f611da85f40bf2d22ee
Luke Bennett <luke.bennett@mdrx.tech>
2024-06-06 18:19:13 +0100

    feat: run until out of messages

--- cleanUp.js
+++ cleanUp.js
@@ -5,6 +5,8 @@ const { OAuth2 } = google.auth;
 
 const destinationLabel = "Archive - 2023 II"
 
+const IGNORE_LABELS = "-{label:archive---2022} -{label:archive---2023} -{label:archive---2023-II} -{label:unsubscribe}"
+
 const oauth2Client = new OAuth2(
   process.env.CLIENT_ID,
   process.env.CLIENT_SECRET,
@@ -35,8 +37,11 @@ async function moveEmailsToArchive() {
 
     const resMessages = await gmail.users.messages.list({
       userId: "me",
-      q: "in:inbox",
+      q: `in:inbox ${IGNORE_LABELS}`,
     });
+
+    console.log('Got messages:', resMessages.data.messages.length);
+    console.log('First message as example:', resMessages.data.messages[0]);
 
     if (!resMessages.data.messages || resMessages.data.messages.length === 0) {
       console.log("No messages found.");
@@ -56,9 +61,24 @@ async function moveEmailsToArchive() {
       });
       console.log(`Message ${message.id} moved to '${destinationLabel}'`);
     }
+
+    return messages.length;
   } catch (error) {
     console.error("Error moving emails:", error);
   }
+
+  return 0;
 }
 
-moveEmailsToArchive();
+
+async function main() {
+  let count = 100;
+
+  while (count > 0) {
+    console.log('TICK');
+    count = await moveEmailsToArchive();
+    console.log('Moved', count, 'messages to archive');
+  }
+}
+
+main();
